4a39215e5f9e2edb53cddf1cf3b3fd5f44b630f1
EdiCasi <80850475+EdiCasi@users.noreply.github.com>
2021-06-16 22:41:14 +0300

    Made the bottom menu functionally, and 4 fragments for the testing proposal

--- app/src/main/java/modelViews/HomeActivity.java
+++ app/src/main/java/modelViews/HomeActivity.java
@@ -1,10 +1,14 @@
 package modelViews;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.user.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity
 {
@@ -14,5 +18,40 @@ public class HomeActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListner);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fl_home_container,new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner =
+            new BottomNavigationView.OnNavigationItemSelectedListener()
+            {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item)
+                {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId())
+                    {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                        case R.id.nav_news:
+                            selectedFragment = new NewsFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fl_home_container,selectedFragment).commit();
+
+                    // If we return false, we do not select the navigation Item
+                    return true;
+                }
+            };
 }
---
+++ app/src/main/java/modelViews/HomeFragment.java
@@ -0,0 +1,23 @@
+package modelViews;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.user.R;
+
+public class HomeFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.home_fragment,container,false);
+
+    }
+}
